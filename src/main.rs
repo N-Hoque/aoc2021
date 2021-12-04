e7468c0e676a26bc6391e93d1e8c875c15e76ada
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-04 00:22:09 +0000

    Added Day 3 to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 use aoc2021::{day_1, day_2};
+=======
+use aoc2021::{day_1, day_2, day_3};
+>>>>>>> 4b4ebc0... Added Day 3 to main.rs
 
 fn main() {
     println!("Day 1!");
@@ -9,5 +13,10 @@ fn main() {
     println!("Day 2!");
     day_2::part_1();
     day_2::part_2();
+    println!();
+
+    println!("Day 3!");
+    day_3::part_1();
+    day_3::part_2();
     println!();
 }
