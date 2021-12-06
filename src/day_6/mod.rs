fd2ad8862859cff0e3d70b7d43337ca74984364d
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-06 01:10:35 +0000

    Started Day 6

---
+++ src/day_6/mod.rs
@@ -0,0 +1,14 @@
+pub fn solve_sample() -> u64 {
+    0
+}
+
+pub fn part_1() -> u64 {
+    0
+}
+
+pub fn part_2() -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/day_6/tests.rs
@@ -0,0 +1,16 @@
+use super::{part_1, part_2, solve_sample};
+
+#[test]
+fn test_solve_sample() {
+    assert_eq!(51034, solve_sample());
+}
+
+#[test]
+fn test_part_1() {
+    assert_eq!(51034, part_1());
+}
+
+#[test]
+fn test_part_2() {
+    assert_eq!(5434, part_2());
+}
