6e2d722978e67313fcb7279419a579a4da4781ba
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-07 17:01:01 +0000

    Started day 7

---
+++ src/day_7/mod.rs
@@ -0,0 +1,26 @@
+pub fn solve_sample() -> u64 {
+    0
+}
+
+pub fn solve_sample_2() -> u64 {
+    0
+}
+
+pub fn part_1() -> u64 {
+    let result = 0;
+
+    println!("{}", result);
+
+    result
+}
+
+pub fn part_2() -> u64 {
+    let result = 0;
+
+    println!("{}", result);
+
+    result
+}
+
+#[cfg(test)]
+mod tests;
