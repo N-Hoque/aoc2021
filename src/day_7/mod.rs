2cc3bce237389424d9ffb7faf9b0019d33682e84
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-07 18:06:39 +0000

    Completed Part 2

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -4,6 +4,11 @@ use crate::read_to_string;
 
 const SAMPLE_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
 
+enum Method {
+    Simple,
+    Summative,
+}
+
 fn get_data(input: &str) -> Vec<u64> {
     input
         .trim()
@@ -12,37 +17,47 @@ fn get_data(input: &str) -> Vec<u64> {
         .collect()
 }
 
-fn solve(source: &str) -> u64 {
+fn solve(source: &str, method: Method) -> u64 {
     let data = get_data(source);
 
-    let mut costs = HashMap::new();
+    let min = *data.iter().min().expect("Cannot find minimum value");
+    let max = *data.iter().max().expect("Cannot find maximum value");
 
-    for x in &data {
-        if costs.contains_key(x) {
-            continue;
-        }
+    let mut minimum_cost = u64::MAX;
+
+    for x in min..=max {
+        let mut current_cost = 0;
         for y in &data {
-            let difference = f64::abs(*x as f64 - *y as f64) as u64;
-            costs
-                .entry(x)
-                .and_modify(|x| *x += difference)
-                .or_insert(difference);
+            let cost = {
+                let difference = f64::abs(x as f64 - *y as f64) as u64;
+                if let Method::Simple = method {
+                    difference
+                } else {
+                    (difference * (difference + 1)) / 2
+                }
+            };
+
+            current_cost += cost;
+        }
+
+        if current_cost < minimum_cost {
+            minimum_cost = current_cost;
         }
     }
 
-    *costs.values().min().expect("No minimum found")
+    minimum_cost
 }
 
 pub fn solve_sample() -> u64 {
-    solve(SAMPLE_INPUT)
+    solve(SAMPLE_INPUT, Method::Simple)
 }
 
 pub fn solve_sample_2() -> u64 {
-    0
+    solve(SAMPLE_INPUT, Method::Summative)
 }
 
 pub fn part_1() -> u64 {
-    let result = solve(&read_to_string("res/day_7.txt"));
+    let result = solve(&read_to_string("res/day_7.txt"), Method::Simple);
 
     println!("Part 1: {}", result);
 
@@ -50,7 +65,7 @@ pub fn part_1() -> u64 {
 }
 
 pub fn part_2() -> u64 {
-    let result = solve(&read_to_string("res/day_7.txt"));
+    let result = solve(&read_to_string("res/day_7.txt"), Method::Summative);
 
     println!("Part 2: {}", result);
 
