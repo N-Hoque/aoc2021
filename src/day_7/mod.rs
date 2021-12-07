ffe688a2da6cd1d3c84773c82f640dcd4028c928
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-07 17:42:39 +0000

    Completed Part 1

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,5 +1,40 @@
+use std::collections::HashMap;
+
+use crate::read_to_string;
+
+const SAMPLE_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+fn get_data(input: &str) -> Vec<u64> {
+    input
+        .trim()
+        .split(',')
+        .map(|x| x.parse::<u64>().expect("Cannot parse value"))
+        .collect()
+}
+
+fn solve(source: &str) -> u64 {
+    let data = get_data(source);
+
+    let mut costs = HashMap::new();
+
+    for x in &data {
+        if costs.contains_key(x) {
+            continue;
+        }
+        for y in &data {
+            let difference = f64::abs(*x as f64 - *y as f64) as u64;
+            costs
+                .entry(x)
+                .and_modify(|x| *x += difference)
+                .or_insert(difference);
+        }
+    }
+
+    *costs.values().min().expect("No minimum found")
+}
+
 pub fn solve_sample() -> u64 {
-    0
+    solve(SAMPLE_INPUT)
 }
 
 pub fn solve_sample_2() -> u64 {
@@ -7,17 +42,17 @@ pub fn solve_sample_2() -> u64 {
 }
 
 pub fn part_1() -> u64 {
-    let result = 0;
+    let result = solve(&read_to_string("res/day_7.txt"));
 
-    println!("{}", result);
+    println!("Part 1: {}", result);
 
     result
 }
 
 pub fn part_2() -> u64 {
-    let result = 0;
+    let result = solve(&read_to_string("res/day_7.txt"));
 
-    println!("{}", result);
+    println!("Part 2: {}", result);
 
     result
 }
--- src/day_7/tests.rs
+++ src/day_7/tests.rs
@@ -2,7 +2,7 @@ use super::{part_1, part_2, solve_sample, solve_sample_2};
 
 #[test]
 fn test_solve_sample() {
-    assert_eq!(0, solve_sample());
+    assert_eq!(37, solve_sample());
 }
 
 #[test]
@@ -12,7 +12,7 @@ fn test_solve_sample_2() {
 
 #[test]
 fn test_part_1() {
-    assert_eq!(360268, part_1());
+    assert_eq!(337488, part_1());
 }
 
 #[test]
