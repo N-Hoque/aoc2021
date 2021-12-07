9821bee1faff163c9b0a863c31e1c6cae84154d7
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-07 20:20:48 +0000

    Started day 8

---
+++ src/day_8/mod.rs
@@ -0,0 +1,26 @@
+pub fn solve_sample() -> u64 {
+    0
+}
+
+pub fn solve_sample_2() -> u64 {
+    0
+}
+
+pub fn part_1() -> u64 {
+    let result = 0;
+
+    println!("Part 1: {}", result);
+
+    result
+}
+
+pub fn part_2() -> u64 {
+    let result = 0;
+
+    println!("Part 2: {}", result);
+
+    result
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/day_8/tests.rs
@@ -0,0 +1,21 @@
+use super::{part_1, part_2, solve_sample, solve_sample_2};
+
+#[test]
+fn test_solve_sample() {
+    assert_eq!(0, solve_sample());
+}
+
+#[test]
+fn test_solve_sample_2() {
+    assert_eq!(0, solve_sample_2());
+}
+
+#[test]
+fn test_part_1() {
+    assert_eq!(0, part_1());
+}
+
+#[test]
+fn test_part_2() {
+    assert_eq!(0, part_2());
+}
