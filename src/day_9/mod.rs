93517fb0e6b3ed7a1f98f84bb68072b7c5d66201
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-11 15:04:26 +0000

    Completed Part 1

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -1,11 +1,93 @@
+use crate::read_to_string;
+
 const SAMPLE_INPUT: &str = "2199943210
 3987894921
 9856789892
 8767896789
 9899965678";
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Point {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Point {
+    pub fn new(x: i64, y: i64, z: i64) -> Self {
+        Self { x, y, z }
+    }
+}
+
+impl std::ops::Sub for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let x = self.x - rhs.x;
+        let y = self.y - rhs.y;
+        let z = self.z - rhs.z;
+        Self { x, y, z }
+    }
+}
+
+fn calculate_risk(minimal_points: &[Point]) -> u64 {
+    minimal_points.iter().map(|p| (p.z + 1) as u64).sum()
+}
+
+fn find_minimal_points(heatmap: &[Point]) -> Vec<Point> {
+    let mut minimal_points = Vec::new();
+
+    for point in heatmap {
+        let neighbors = find_neighbors(heatmap, point);
+        if neighbors.iter().all(|p| point.z < p.z) {
+            minimal_points.push(*point);
+        }
+    }
+
+    minimal_points
+}
+
+fn find_neighbors(heatmap: &[Point], from: &Point) -> Vec<Point> {
+    let mut neighbors = Vec::new();
+
+    for point in heatmap {
+        let delta = *point - *from;
+        match delta {
+            Point { x: 1, y: 0, .. } => neighbors.push(*point),
+            Point { x: -1, y: 0, .. } => neighbors.push(*point),
+            Point { x: 0, y: 1, .. } => neighbors.push(*point),
+            Point { x: 0, y: -1, .. } => neighbors.push(*point),
+            _ => continue,
+        }
+    }
+
+    neighbors
+}
+
+fn generate_heatmap(input: &str) -> Vec<Point> {
+    let mut points = Vec::new();
+
+    for (idx, line) in input.lines().enumerate() {
+        for (jdx, ch) in line.chars().enumerate() {
+            points.push(Point::new(
+                idx as i64,
+                jdx as i64,
+                ch.to_digit(10).expect("Cannot convert char to digit") as i64,
+            ));
+        }
+    }
+
+    points
+}
+
 fn solve_sample() -> u64 {
-    0
+    let heatmap = generate_heatmap(SAMPLE_INPUT);
+    let minimal_points = find_minimal_points(&heatmap);
+    let risk_level = calculate_risk(&minimal_points);
+
+    println!("Sample 1: {}", risk_level);
+
+    risk_level
 }
 
 fn solve_sample_2() -> u64 {
@@ -13,7 +95,14 @@ fn solve_sample_2() -> u64 {
 }
 
 pub fn part_1() -> u64 {
-    0
+    let data = read_to_string("res/day_9.txt");
+    let heatmap = generate_heatmap(&data);
+    let minimal_points = find_minimal_points(&heatmap);
+    let risk_level = calculate_risk(&minimal_points);
+
+    println!("Part 1: {}", risk_level);
+
+    risk_level
 }
 
 pub fn part_2() -> u64 {
