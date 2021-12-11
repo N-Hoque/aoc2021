aa1681a2591f2d9cab890f2041fe5346e824632a
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-11 16:07:32 +0000

    Wrapped logic in HeatmapAnalyser struct

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -34,115 +34,136 @@ impl std::ops::Sub for Point {
     }
 }
 
-fn calculate_risk(heatmap: &[Point]) -> u64 {
-    let minimal_points = find_minimal_points(heatmap);
-
-    minimal_points.iter().map(|p| (p.z + 1) as u64).sum()
+struct HeatmapAnalyser {
+    heatmap: Vec<Point>,
 }
 
-fn find_basin(heatmap: &[Point], minimal_point: &Point) -> Vec<Point> {
-    let mut explored = vec![*minimal_point];
+impl HeatmapAnalyser {
+    pub fn new(source: &str) -> Self {
+        let data = read_to_string(source);
+        Self {
+            heatmap: HeatmapAnalyser::generate_heatmap(&data),
+        }
+    }
 
-    let initial_neighbors = find_neighbors(heatmap, minimal_point)
-        .iter()
-        .filter(|p| p.z != 9)
-        .copied()
-        .collect_vec();
+    pub fn with_sample() -> Self {
+        Self {
+            heatmap: HeatmapAnalyser::generate_heatmap(SAMPLE_INPUT),
+        }
+    }
 
-    let mut unexplored = VecDeque::from_iter(initial_neighbors.iter().copied());
+    fn generate_heatmap(input: &str) -> Vec<Point> {
+        let mut points = Vec::new();
 
-    while let Some(point_to_search) = unexplored.pop_front() {
-        let new_neighbors = find_neighbors(heatmap, &point_to_search)
+        for (idx, line) in input.lines().enumerate() {
+            for (jdx, ch) in line.chars().enumerate() {
+                points.push(Point::new(
+                    idx as i64,
+                    jdx as i64,
+                    ch.to_digit(10).expect("Cannot convert char to digit") as i64,
+                ));
+            }
+        }
+
+        points
+    }
+
+    pub fn calculate_risk(&self) -> u64 {
+        let minimal_points = self.find_minimal_points();
+
+        minimal_points.iter().map(|p| (p.z + 1) as u64).sum()
+    }
+
+    pub fn calculate_basin_strength(&self) -> u64 {
+        let basins = self.find_basins();
+        let max_basin_size = basins
+            .iter()
+            .take(3)
+            .map(|b| b.len() as u64)
+            .product::<u64>();
+        max_basin_size
+    }
+
+    fn find_basin(&self, minimal_point: &Point) -> Vec<Point> {
+        let mut explored = vec![*minimal_point];
+
+        let initial_neighbors = self
+            .find_neighbors(minimal_point)
             .iter()
             .filter(|p| p.z != 9)
             .copied()
             .collect_vec();
-        explored.push(point_to_search);
-        for point in new_neighbors {
-            if explored.contains(&point) || unexplored.contains(&point) {
-                continue;
+
+        let mut unexplored = VecDeque::from_iter(initial_neighbors.iter().copied());
+
+        while let Some(point_to_search) = unexplored.pop_front() {
+            let new_neighbors = self
+                .find_neighbors(&point_to_search)
+                .iter()
+                .filter(|p| p.z != 9)
+                .copied()
+                .collect_vec();
+            explored.push(point_to_search);
+            for point in new_neighbors {
+                if explored.contains(&point) || unexplored.contains(&point) {
+                    continue;
+                }
+                unexplored.push_back(point);
             }
-            unexplored.push_back(point);
         }
+
+        let explored = explored.iter().copied().collect_vec();
+
+        explored
     }
 
-    let explored = explored.iter().copied().collect_vec();
+    fn find_basins(&self) -> Vec<Vec<Point>> {
+        let minimal_points = self.find_minimal_points();
+        let mut basins = Vec::new();
 
-    explored
-}
-
-fn find_basins(heatmap: &[Point]) -> Vec<Vec<Point>> {
-    let minimal_points = find_minimal_points(heatmap);
-    let mut basins = Vec::new();
-
-    for point in minimal_points {
-        basins.push(find_basin(heatmap, &point));
-    }
-
-    basins.sort_by_key(|b2| std::cmp::Reverse(b2.len()));
-
-    basins
-}
-
-fn find_minimal_points(heatmap: &[Point]) -> Vec<Point> {
-    let mut minimal_points = Vec::new();
-
-    for point in heatmap {
-        let neighbors = find_neighbors(heatmap, point);
-        if neighbors.iter().all(|p| point.z < p.z) {
-            minimal_points.push(*point);
+        for point in minimal_points {
+            basins.push(self.find_basin(&point));
         }
+
+        basins.sort_by_key(|b2| std::cmp::Reverse(b2.len()));
+
+        basins
     }
 
-    minimal_points
-}
+    fn find_minimal_points(&self) -> Vec<Point> {
+        let mut minimal_points = Vec::new();
 
-fn find_neighbors(heatmap: &[Point], from: &Point) -> Vec<Point> {
-    let mut neighbors = Vec::new();
-
-    for point in heatmap {
-        let delta = *point - *from;
-        match delta {
-            Point { x: 1, y: 0, .. } => neighbors.push(*point),
-            Point { x: -1, y: 0, .. } => neighbors.push(*point),
-            Point { x: 0, y: 1, .. } => neighbors.push(*point),
-            Point { x: 0, y: -1, .. } => neighbors.push(*point),
-            _ => continue,
+        for point in &self.heatmap {
+            let neighbors = self.find_neighbors(point);
+            if neighbors.iter().all(|p| point.z < p.z) {
+                minimal_points.push(*point);
+            }
         }
+
+        minimal_points
     }
 
-    neighbors
-}
+    fn find_neighbors(&self, from: &Point) -> Vec<Point> {
+        let mut neighbors = Vec::new();
 
-fn calculate_basin_strength(heatmap: &[Point]) -> u64 {
-    let basins = find_basins(heatmap);
-    let max_basin_size = basins
-        .iter()
-        .take(3)
-        .map(|b| b.len() as u64)
-        .product::<u64>();
-    max_basin_size
-}
-
-fn generate_heatmap(input: &str) -> Vec<Point> {
-    let mut points = Vec::new();
-
-    for (idx, line) in input.lines().enumerate() {
-        for (jdx, ch) in line.chars().enumerate() {
-            points.push(Point::new(
-                idx as i64,
-                jdx as i64,
-                ch.to_digit(10).expect("Cannot convert char to digit") as i64,
-            ));
+        for point in &self.heatmap {
+            let delta = *point - *from;
+            match delta {
+                Point { x: 1, y: 0, .. } => neighbors.push(*point),
+                Point { x: -1, y: 0, .. } => neighbors.push(*point),
+                Point { x: 0, y: 1, .. } => neighbors.push(*point),
+                Point { x: 0, y: -1, .. } => neighbors.push(*point),
+                _ => continue,
+            }
         }
-    }
 
-    points
+        neighbors
+    }
 }
 
 fn solve_sample() -> u64 {
-    let heatmap = generate_heatmap(SAMPLE_INPUT);
-    let risk_level = calculate_risk(&heatmap);
+    let heatmap_analyser = HeatmapAnalyser::with_sample();
+    let risk_level = heatmap_analyser.calculate_risk();
 
     println!("Sample 1: {}", risk_level);
 
@@ -150,8 +171,8 @@ fn solve_sample() -> u64 {
 }
 
 fn solve_sample_2() -> u64 {
-    let heatmap = generate_heatmap(SAMPLE_INPUT);
-    let max_basin_size = calculate_basin_strength(&heatmap);
+    let heatmap_analyser = HeatmapAnalyser::with_sample();
+    let max_basin_size = heatmap_analyser.calculate_basin_strength();
 
     println!("Sample 2: {}", max_basin_size);
 
@@ -159,9 +180,8 @@ fn solve_sample_2() -> u64 {
 }
 
 pub fn part_1() -> u64 {
-    let data = read_to_string("res/day_9.txt");
-    let heatmap = generate_heatmap(&data);
-    let risk_level = calculate_risk(&heatmap);
+    let heatmap_analyser = HeatmapAnalyser::new("res/day_9.txt");
+    let risk_level = heatmap_analyser.calculate_risk();
 
     println!("Part 1: {}", risk_level);
 
@@ -169,13 +189,12 @@ pub fn part_1() -> u64 {
 }
 
 pub fn part_2() -> u64 {
-    let data = read_to_string("res/day_9.txt");
-    let heatmap = generate_heatmap(&data);
-    let max_basin_size = calculate_basin_strength(&heatmap);
+    let heatmap_analyser = HeatmapAnalyser::new("res/day_9.txt");
+    let basin_strength = heatmap_analyser.calculate_basin_strength();
 
-    println!("Part 2: {}", max_basin_size);
+    println!("Part 2: {}", basin_strength);
 
-    max_basin_size
+    basin_strength
 }
 
 #[cfg(test)]
