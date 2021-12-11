60ddacaebaa08c63180255373ef08eb850f46361
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-11 17:06:17 +0000

    Completed Part 1

--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,3 +1,7 @@
+use std::collections::VecDeque;
+
+use crate::read_to_string;
+
 const SAMPLE_INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
@@ -9,8 +13,55 @@ const SAMPLE_INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]";
 
+fn find_illegal_bracket(line: &str) -> Option<char> {
+    let mut bracket_stack = VecDeque::new();
+
+    let line_length = line.len();
+    for (idx, ch) in line.chars().enumerate() {
+        if idx == line_length - 1 || ch == '[' || ch == '{' || ch == '(' || ch == '<' {
+            bracket_stack.push_back(ch);
+        } else {
+            match (ch, bracket_stack.pop_back()) {
+                (']', Some(b)) if b != '[' => {
+                    return Some(ch);
+                }
+                ('}', Some(b)) if b != '{' => {
+                    return Some(ch);
+                }
+                (')', Some(b)) if b != '(' => {
+                    return Some(ch);
+                }
+                ('>', Some(b)) if b != '<' => {
+                    return Some(ch);
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    None
+}
+
+fn get_illegal_bracket_score(bracket: char) -> u64 {
+    match bracket {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        ' ' => 0,
+        _ => panic!("Invalid bracket given: {}", bracket),
+    }
+}
+
 pub fn solve_sample() -> u64 {
-    0
+    let total_score = SAMPLE_INPUT
+        .lines()
+        .filter_map(|l| find_illegal_bracket(l).map(get_illegal_bracket_score))
+        .sum();
+
+    println!("Sample 1: {}", total_score);
+
+    total_score
 }
 
 pub fn solve_sample_2() -> u64 {
@@ -18,7 +69,16 @@ pub fn solve_sample_2() -> u64 {
 }
 
 pub fn part_1() -> u64 {
-    0
+    let data = read_to_string("res/day_10.txt");
+
+    let total_score = data
+        .lines()
+        .filter_map(|l| find_illegal_bracket(l).map(get_illegal_bracket_score))
+        .sum();
+
+    println!("Part 1: {}", total_score);
+
+    total_score
 }
 
 pub fn part_2() -> u64 {
